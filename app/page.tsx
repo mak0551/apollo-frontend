a72a4997a2a3345d9f1d4fa382121cbfa6fd363d
mohammed ahmed khan <84978114+mak0551@users.noreply.github.com>
2025-05-03 15:22:19 +0530

    url fix

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
     async function fetchDoctors() {
       try {
         const res = await fetch(
-          "https://apollo-server-wjvc.onrender.com//doctor/getall?page=1&limit=20",
+          "https://apollo-server-wjvc.onrender.com/doctor/getall?page=1&limit=20",
           {
             cache: "no-store",
           }
